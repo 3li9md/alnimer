52fb387c66c865065808d56ea5b10ce7699e9d15
3li9md <49075636+3li9md@users.noreply.github.com>
2022-06-28 17:19:30 +0300

    Version 3.1
    - Fixed inner movie page with more details
    - Mocked moviesList details to sense the experience

--- src/app/shared/MoviesList.ts
+++ src/app/shared/MoviesList.ts
@@ -4,81 +4,81 @@ export const MOVIES: Movie[] = [
     {
         id: 1,
         title: 'Memory of Tomorrow',
-        text: 'lorem',
+        text: 'When time starts to clock down, Ahmed strives to find the root of every misfortune he encounters to prevent it from happening.',
         img: 'assets/images/posters/memory.jpg',
         genre: 'Thriller',
         duration: "45 mins",
-        ageRating: "string",
+        ageRating: "+13",
         rating: 8.5
     },
     {
         id: 2,
         title: 'Faces on the Table',
-        text: 'lorem',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque impedit exercitationem',
         img: 'assets/images/posters/faces.jpg',
         genre: 'Drama',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        duration: "1hr 15mins",
+        ageRating: "+13",
+        rating: 7.9
     },
     {
         id: 3,
         title: 'Into the Silence',
-        text: 'lorem',
+        text: 'As their mission becomes more sophisticated to be achieved by humans, a research team develops a pair of crows genetically to puzzle out the case.',
         img: 'assets/images/posters/silence.jpg',
         genre: 'Sci-fi',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        duration: "58 mins",
+        ageRating: "PG",
+        rating: 8.0
     },
     {
         id: 4,
         title: 'Anomalies',
-        text: 'lorem',
+        text: 'A gravitational imbalance pulls a private plane carrying scientists to land involuntarily in a place out of service, forcing them to seek after the reason for this incident themselves.',
         img: 'assets/images/posters/anomalies.jpg',
         genre: 'Sci-fi',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        duration: "1hr 30mins",
+        ageRating: "+13",
+        rating: 8.2
     },
     {
         id: 5,
         title: 'Building 23',
-        text: 'lorem',
+        text: 'A legendary place that lies in the core of a mountain is said to have the only solution to a scientific dilemma, takes a student on a critical fleeting journey to discover its path.',
         img: 'assets/images/posters/building23.jpg',
         genre: 'Adventure',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        duration: "25 mins",
+        ageRating: "PG",
+        rating: 6.8
     },
     {
         id: 6,
         title: 'I Need Your Forgivness',
-        text: 'lorem',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cumque impedit exercitationem',
         img: 'assets/images/posters/forgivness.jpg',
         genre: 'Drama',
         duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        ageRating: "+13",
+        rating: 8.4
     },
     {
         id: 7,
         title: 'Rhapsody of Things',
-        text: 'lorem',
+        text: 'A musical chase of two birds over a neighborhood at different points in time, revealing a surprising perspective of life.',
         img: 'assets/images/posters/rhapsody.jpg',
-        genre: 'Poetic',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        genre: 'Musical',
+        duration: "30 mins",
+        ageRating: "PG",
+        rating: 9.2
     },
     {
         id: 8,
         title: 'Eradication',
-        text: 'lorem',
+        text: 'A hitman gets blackmailed to keep doing the filthy business of a secret organization, so he decides to go after their leader.',
         img: 'assets/images/posters/eradication.jpg',
         genre: 'Thriller',
-        duration: "45 mins",
-        ageRating: "string",
-        rating: 8.5
+        duration: "1hr 15mins",
+        ageRating: "+18",
+        rating: 7.3
     },
 ];
