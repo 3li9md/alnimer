0a641626ca70bf1e7051a7cdc466f86758541a45
3li9md <49075636+3li9md@users.noreply.github.com>
2022-06-29 17:15:34 +0300

    Version 4
    A recovered version after a catastrophic

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -9,6 +9,29 @@ export class AboutPageComponent implements OnInit {
 
   constructor() { }
 
+  info: { title: string, icon: string, text: string }[] = [
+    {
+      title: "Born and Raised",
+      icon: "assets/images/icons/user.png",
+      text: "Ali Alnimer, born & raised in Dammam, Saudi Arabia, Jan 1999."
+    },
+    {
+      title: "Education",
+      icon: "assets/images/icons/mortarboard.png",
+      text: "Bachelor of Science Degree in Software Engineering from King Fahd University of Petroleum & Minerals in Dhahran, Saudi Arabia, Jan 2023."
+    },
+    {
+      title: "Career Path",
+      icon: "assets/images/icons/route.png",
+      text: " My elder enthusiasm for Graphic Design slipped me into this field of study, specifically by starting to explore UI & UX Design principles. "
+    },
+    {
+      title: "sth else",
+      icon: "assets/images/icons/clapperboard.png",
+      text: "Lorem and his siblings"
+    },
+  ]
+
   ngOnInit(): void {
   }
 
